b355b7cff4253974f5c446ad0e13b70eb690d14d
Dave Olsen <dmolsen@gmail.com>
2014-06-27 14:16:47 -0400

    initial commit

---
+++ assets/js/layout.js
@@ -0,0 +1,5 @@
+/* load pattern nav */
+var template         = document.getElementById("pl-pattern-nav-template");
+var templateCompiled = Hogan.compile(template.innerHTML);
+var templateRendered = templateCompiled.render(navItems);
+document.getElementById("pl-pattern-nav-target").innerHTML(templateRendered);
