dd3beeaa2c21661eb26384842ccb9587d9bd214f
Dave Olsen <dmolsen@gmail.com>
2014-06-27 16:51:21 -0400

    properly rendering the nav

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -2,4 +2,4 @@
 var template         = document.getElementById("pl-pattern-nav-template");
 var templateCompiled = Hogan.compile(template.innerHTML);
 var templateRendered = templateCompiled.render(navItems);
-document.getElementById("pl-pattern-nav-target").innerHTML(templateRendered);
+document.getElementById("pl-pattern-nav-target").innerHTML = templateRendered;
